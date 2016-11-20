21c4d7d65fec66674cf6789ac66883e36bd0345a
Chris Zosh <Chris Zosh>
2016-11-20 16:21:46 -0500

    Added files

---
+++ Chess.cpp
@@ -0,0 +1,147 @@
+#include "Chess.hpp"
+using namespace std;
+
+Board::Board() {
+	//set the starting position here
+}
+
+Board::Board(const Board& b) {
+	for (int i = 0; i<8; ++i) {
+		for (int j = 0; j<8; ++j) {
+			pieces[i][j] = b.pieces[i][j];
+		}
+	}
+}
+
+Game::Game() {
+    enPassant[0] = enPassant[1] = Coord(-1,-1); //set them to an impossible square so they never match
+    for (int i = 0; i<2; ++i) {
+		kingMoved[i] = false;
+		for (int j = 0; j<2; ++j) {
+			rookMoved[i][j] = false;
+		}
+    }
+}
+
+bool Game::onBoard(Coord pos) {
+	return (pos.x>=0 && pos.x<=7 && pos.y>=0 && pos.y<=7);
+}
+
+Color Game::squareOccupied(Board& white, Board& black, Coord pos) {
+	if (!onBoard(pos))
+		return None;
+
+    if (white[pos.x][pos.y]!=Empty)
+		return White;
+
+	if (black[pos.x][7-pos.y]!=Empty) //black pos is y-inverted to flip the board
+		return Black;
+
+	return None;
+}
+
+vector<Coord> Game::getControlledSquares(Board& white, Board& black, Color color, Coord pos) {
+	Board* board[2] = {&white,&black}; //we do this to get both boards in one variable to make our code concise. (*board[COLOR])[x][y] is how to use it
+	vector<Coord> squares;
+    Piece piece;
+    Color enemy = (color==White)?(Black):(White); //the color of the opponent
+    Coord check;
+
+    if (color!=White && color!=Black) //return empty list if we get a bad color
+		return squares;
+	piece = (*board[color])[pos.x][pos.y];
+
+    switch (piece) {
+		case Pawn:
+			//check square in front. If pos.y==6 then also check the double push. Check the capture squares as well.
+			//push squares are added if they are empty. Capture squares are added if they have the opposite color piece
+			//don't forget to also check the en passant squares. Meaning, the capture squares are legal if they are occupied or if their position is equal to one of the en passant squares
+
+			check = Coord(pos.x,pos.y-1); //Can the
+			if (squareOccupied(*board[White],*board[Black],check)==None && onBoard(check))
+                squares.push_back(check);
+
+            check = Coord(pos.x,pos.y-2)
+
+            check = Coord(pos.x-1,pos.y-1);//capture diagnal
+            if (squareOccupied(*board[White],*board[Black],check) == enemy && onBoard(check))
+                squares.push_back(square);
+            check = Coord(pos.x-1,pos.y-1);
+            if (squareOccupied(*board[white],*board[Black],check) == enemy && onBoard(check))
+                squares.push_back(square);
+
+
+
+			break;
+
+		case Rook:
+			//traverse up down left and right until a square is occupied or off the board.
+			//add occupied squares if they are enemy
+			break;
+
+		case Knight:
+			//check squares matching the knight pattern (dx==2,dy==1 or dx==1,dy==2). Might be easiest to just have a whole if statement for each case (there are 8)
+			//empty squares and enemy occupied squares get added
+			break;
+
+		case Bishop:
+			//loop through the diagonal squares until we hit an occupied square or the edge of the board
+			//occupied squares get added if they are enemy
+			break;
+
+		case Queen:
+			//loop through diagonals and corridors until we hit an occupied square or the edge of the board
+			//occupied squares get added if they are enemy
+			break;
+
+		case King:
+			//same deal, you should get this by now
+			//we will handle castling in a different function
+			break;
+
+		default:
+			break;
+    }
+
+    return squares;
+}
+
+bool Game::inCheck(Board& white, Board& black, Color color) {
+	Board* board[2] = {&white,&black};
+	Color enemy = (color==White)?(Black):(White);
+	Coord kingPos;
+	vector<Coord> dangerSquares;
+
+	//loop through all squares and find the position of our king
+	//in addition, loop through all squares on the enemy board and call getControlledSquares on each piece we hit. Aggregate all returned results into dangerSquares
+	//now go through each danger square, if any one of them matches kingPos we are in check, so return true
+
+	return false;
+}
+
+bool Game::moveIsLegal(Color color, Coord oPos, Coord, nPos)
+{
+    Piece piece = pieces[color][oPos.x][oPos.y];
+    vector<Coord> posSquares = getControlledSquares(pieces[White],pieces[Black],color,oPos);
+
+    //loop through posSquares. If one matches nPos, make a temp Board. Put the piece in nPos and call inCheck. If not in check, the move is legal
+    //note, the beautiful thing about this design is that en passant and the double pawn push are included in getControlledSquares so there's no need to explicitly check for them here
+
+    //if we got no matches from the above check for castling
+    //to do this verify that all squares between the rook and king are empty
+    //we also need to verify that the king has not moved, nor the rook we want. Luckily, we have variables that tell us that
+    //we also need to verify that we are not in check, this is as easy as calling inCheck on the current position
+    //one last thing: verify that we do not pass through check. This can either be done by making a temp board and putting the king on each square (one at a time) and calling inCheck for each one, or by getting a list of all squares controlled by enemy pieces and seeing if any of them are the ones we pass through
+    //if all of the above pass, the castle is legal
+
+    return false;
+}
+
+bool Game::makeMove(Color color, Coord oPos, Coord nPos, Piece promotion)
+{
+	//verify that the move is legal. Return false if not
+	//if it is, set the new square equal to the piece and the old square to Empty
+	//if the move is pawn reaching the back rank we will set the new square to promotion instead of Pawn
+
+	return true;
+}
---
+++ Chess.hpp
@@ -0,0 +1,143 @@
+#ifndef CHESS_HPP
+#define CHESS_HPP
+
+#include <vector>
+
+/**
+ * Helper class to store coordinates in a readable way
+ */
+struct Coord {
+	int x,y; //both in the range [0,7]
+
+	/**
+	 * Default constructor. Initializes (x,y) to (0,0)
+	 */
+	Coord() { x = 0; y = 0; }
+
+	/**
+	 * Initialization constructor
+	 *
+	 * \param _x The x coordinate
+	 * \param y The y coordinate
+	 */
+	Coord(int _x, int _y) { x = _x; _y = y; }
+};
+
+/**
+ * Basic enum for representing each type of piece
+ */
+enum Piece {
+	Empty,
+	Pawn,
+	Rook,
+	Knight,
+	Bishop,
+	Queen,
+	King
+};
+
+/**
+ * Enum for representing the color of the pieces
+ */
+enum Color {
+	White = 0,
+	Black = 1,
+	None = -1
+};
+
+/**
+ * Container class for storing pieces. This is kept separate from Game so temporary positions can be created to help determine if moves are legal
+ */
+struct Board {
+	Piece pieces[8][8];
+
+	/**
+	 * Default constructor. Sets the board to the starting position
+	 */
+	Board();
+
+	/**
+	 * Copy constructor. Copies the contents of the given board into this one
+	 *
+	 * \param b The board to copy
+	 */
+	Board(const Board& b);
+};
+
+/**
+ * Board class for storing the position and determining legal moves
+ */
+class Game {
+	Board pieces[2]; //pieces[White] are the white pieces, pieces[Black] are the black pieces. Each side's array will have their pieces at the bottom, the reason for this is so the functions that determine moves don't have to be rewritten for each color
+	Coord enPassant[2][2]; //if a pawn is double pushed, set these 2 positions to the squares next to the pawn. They will be used the next turn to determine if en passant is legal. First index is color
+    bool rookMoved[2][2]; //use these for castling. The first index is the color, the second index is which rook (A = 0, H = 1)
+    bool kingMoved[2]; //also use this for castling. Index is color
+
+    /**
+     * Determines if the given coordinate is on the board
+     *
+     * \param pos The position to check
+     * \return True if the square is on the board, false otherwise
+     */
+	bool onBoard(Coord pos);
+
+	/**
+	 * Determines if a square is occupied. This function is a helper function for getControlledSquares
+	 *
+	 * \param white The white pieces
+	 * \param black The black pieces
+	 * \param pos The square to check for pieces
+	 * \return The color of the piece on the square, or None if empty or if the position is outside of the board
+	 */
+	Color squareOccupied(Board& white, Board& black, Coord pos);
+
+	/**
+	 * Determines all of the squares that the given piece controls. This includes empty squares as well as enemy occupied squares that they can capture on. Note, not all moves may be legal, this function does not take into account pins or check
+	 *
+	 * \param white The white pieces
+	 * \param black The black pieces
+	 * \param color The color of the piece to use
+	 * \param pos The position of the piece. The type of piece will be determined by what is on that square
+	 * \return A vector containing all squares that the piece controls
+	 */
+    std::vector<Coord> getControlledSquares(Board& white, Board& black, Color color, Coord pos);
+
+    /**
+     * Determines if the given side is in check or not. This function takes a Board argument so that temporary positions can be created to easily check for pins
+     *
+     * \param white The white pieces
+     * \param black The black pieces
+     * \param color The color to test for being in check
+     * \return True if the side is in check, false otherwise
+     */
+	bool inCheck(Board& white, Board& black, Color color);
+
+public:
+	/**
+	 * Default constructor. Initializes the en passant squares and the castling variables
+	 */
+	Game();
+
+	/**
+	 * Determines if a move is legal or not. Handles all special cases like castling, en passant, double/single pawn push, check, etc
+	 *
+	 * \param color The color of the piece to check for
+	 * \param oPos The current position of the piece
+	 * \param nPos The position to move the piece to
+	 * \return True if the move is legal, false otherwise
+	 */
+	bool moveIsLegal(Color color, Coord oPos, Coord, nPos);
+
+	/**
+	 * Makes a move on the board if it is legal
+	 *
+	 * \param color The color of the piece to move
+	 * \param oPos The current square of the piece
+	 * \param nPos The square to move to
+	 * \param promotion If the move is a pawn making it to the back rank it will become this piece
+	 * \return True if the piece was moved, false otherwise
+	 */
+	bool makeMove(Color color, Coord oPos, Coord nPos, Piece promotion = Empty);
+};
+
+#endif // CHESS_HPP
