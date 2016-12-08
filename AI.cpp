dec13e1196a87506941846d6e07ab1251255343e
Ben <reidben24@gmail.com>
2016-12-07 20:02:36 -0500

    Added some files for a basic AI

---
+++ AI.cpp
@@ -0,0 +1,8 @@
+#include "AI.hpp"
+using namespace std;
+
+AI::AI(Chess& b, Color m)
+{
+	board = b;
+	me = m;
+}
---
+++ AI.hpp
@@ -0,0 +1,41 @@
+#ifndef AI_HPP
+#define AI_HPP
+
+#include "Chess.hpp"
+
+/**
+ * For now this essentially wraps a function. Later maybe it can store trees and optimize by only
+ * needing to search down one level more as moves are made
+ */
+class AI
+{
+    static const int depth = 3;
+
+    Chess& board;
+    Color me;
+
+public:
+	/**
+     * Default constructor
+     *
+     * \param b A reference to the board of play
+     * \param m The color to play as
+     */
+	AI(Chess& b, Color m);
+
+	/**
+	 * Evaluates the position based on material and determines a move
+	 *
+	 * \return A pair containing the Coord of the piece to move and another Coord to move it to
+	 */
+	std::pair<Coord,Coord> getMove();
+
+	/**
+	 * Returns a random legal move
+	 *
+	 * \return A pair containing the Coord of the piece to move and another Coord to move it to
+	 */
+	std::pair<Coord,Coord> getRandomMove();
+};
+
+#endif // AI_HPP
